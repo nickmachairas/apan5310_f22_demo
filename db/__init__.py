f4dc63d9a1b89527912e1ce10318bf8778fb9a7d
Nick Machairas <nick@machairas.com>
2022-11-29 12:10:32 -0800

    restart

--- db/__init__.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-
-engine = create_engine("postgresql://postgres:123@localhost/etl_demo")
-Base = declarative_base()
